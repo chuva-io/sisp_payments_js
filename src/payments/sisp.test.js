88e984b176007903ebab956862c2ca00b62e2a85
Fred Soares <29602236+FredSoares@users.noreply.github.com>
2021-09-08 16:58:19 -0100

    SPJ-6: Create unit test that ensures the service will work as expected #6

---
+++ src/payments/sisp.test.js
@@ -0,0 +1,86 @@
+const sisp = require('./sisp');
+
+describe('SISP Payment', () => {
+  describe('processing webhook', () => {
+    describe('successful payment', () => {
+      let successData;
+
+      beforeEach(() => {
+        successData = {
+          languageMessages: 'pt',
+          merchantResp: 'C',
+          merchantRespAdditionalErrorMessage: '',
+          merchantRespCP: '1047',
+          merchantRespClientReceipt: '',
+          merchantRespEntityCode: '',
+          merchantRespErrorCode: '',
+          merchantRespErrorDescription: '',
+          merchantRespErrorDetail: '',
+          merchantRespMerchantRef: '52',
+          merchantRespMerchantSession: 'S20200626121231',
+          merchantRespMessageID: 'Zo23nJ2P9C3i3e5Z70K1',
+          merchantRespPan: '************4242',
+          merchantRespPurchaseAmount: '400.00',
+          merchantRespReferenceNumber: '',
+          merchantRespReloadCode: '',
+          merchantRespTid: '00004787',
+          merchantRespTimeStamp: '2020-06-26 12:11:01',
+          messageType: '8',
+          resultFingerPrint: 'GXtUrKOwPtZwTbGiuDEjERoPvsmAUOCB9HRC2Smr+/VocZVluiTzmgbquPJpjPz3yuW0Ouhz05lRWUfF02mJtA==',
+          resultFingerPrintVersion: '1'
+        };
+      });
+
+      it('should return the correct response', async () => {
+        const result = await sisp.processWebhook(successData);
+        const expected = { code: 200 };
+
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('handle SISP payment', () => {
+    const requestBody1 = {
+      merchantRespMerchantRef: '199:301:176'
+    };
+
+    const requestBody2 = {
+      merchant_ref: '199:301:176'
+    };
+
+    const requestBody3 = {
+      merchantRef: '199:301:176'
+    };
+
+    const expected = {
+      store_id: '199',
+      order_id: '301',
+      ref: '176'
+    };
+
+    it('should return the correct response merchantRespMerchantRef', async () => {
+      const result = await sisp.orderDataFromWebhook(requestBody1);
+
+      expect(result).toEqual(
+        expect.objectContaining(expected)
+      );
+    });
+
+    it('should return the correct response merchant_ref', async () => {
+      const result = await sisp.orderDataFromWebhook(requestBody2);
+
+      expect(result).toEqual(
+        expect.objectContaining(expected)
+      );
+    });
+
+    it('should return the correct response merchantRef', async () => {
+      const result = await sisp.orderDataFromWebhook(requestBody3);
+
+      expect(result).toEqual(
+        expect.objectContaining(expected)
+      );
+    });
+  });
+});
