10808223f1f90d28dafa7c8399427e7a24813df3
Luis Fortes <luiscsfortes@Luiss-MacBook-Air.local>
2022-11-22 12:48:35 -0100

    SPJ-43: As a user I would like to be able to make payments using tokenization

--- src/constants/transactionCodes.js
+++ src/constants/transactionCodes.js
@@ -1,7 +1,10 @@
 const TRANSACTION_CODES = {
   purchase: '1',
   service_payment: '2',
-  phone_recharge: '3'
+  phone_recharge: '3',
+  token_enrollment: '5',
+  token_purchase: '6',
+  token_cancel: '7'
 };
 
 module.exports = TRANSACTION_CODES;
